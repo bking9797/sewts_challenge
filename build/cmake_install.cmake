6797d5ab447918938e7e7a47ce4171a7788e43ee
bryce <bryce.king@outlook.de>
2024-08-14 00:48:25 +0200

    gazebo_ros_control plugin added to urdf files

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/bryce/moveit_ws/build/gtest/cmake_install.cmake")
+  include("/home/bryce/moveit_ws/build/fanuc_moveit_sim/cmake_install.cmake")
   include("/home/bryce/moveit_ws/build/fanuc/cmake_install.cmake")
 
 endif()
--- build/fanuc_moveit_sim/CTestTestfile.cmake
+++ build/fanuc_moveit_sim/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/bryce/moveit_ws/src
-# Build directory: /home/bryce/moveit_ws/build
+# Source directory: /home/bryce/moveit_ws/src/fanuc_moveit_sim
+# Build directory: /home/bryce/moveit_ws/build/fanuc_moveit_sim
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("fanuc_moveit_sim")
-subdirs("fanuc")
